bb92148cf35c249a46dcdeb64d6bcf78080b8e04
mark <jdmaykl7@gmail.com>
2023-07-11 13:34:30 +0300

    hm_7

---
+++ HM_7/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_7
+{
+    public class Animal
+    {
+        private string name;
+
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("The animal makes a sound.");
+        }
+    }
+}
